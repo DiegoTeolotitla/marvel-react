75e773466293229bc9ab063e6312003286edce34
Diego Armando Teolotitla Jiménez <dateolotitlaj@truper.com>
2023-04-25 20:44:37 -0600

    Se agrega estilo al menu header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 import Home from "./pages/Home";
 import Series from "./pages/Series";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import "./Header.scss"
+import { useState } from "react";
+import "./Header.scss";
 import { Menu } from "semantic-ui-react";
 import { useLocation, useNavigate } from "react-router-dom";
 export default function Header() {
@@ -16,9 +16,9 @@ export default function Header() {
     return(
         <div className="header-menu">
             <Menu secondary>
-                <Menu.Item name="inicio" active={activeItem === 'inicio'} onClick={handleItemClick}/>
-                <Menu.Item name="series" active={activeItem === 'series'} onClick={handleItemClick}/>
-                <Menu.Item name="comics" active={activeItem === 'comics'} onClick={handleItemClick}/>
+                <Menu.Item name='inicio' active={activeItem === 'inicio'} onClick={handleItemClick}/>
+                <Menu.Item name='series' active={activeItem === 'series'} onClick={handleItemClick}/>
+                <Menu.Item name='comics' active={activeItem === 'comics'} onClick={handleItemClick}/>
             </Menu>
         </div>
     )
