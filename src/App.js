362d7e70fb957453dbc55a14065a5889648e679f
Diego Armando Teolotitla Jiménez <dateolotitlaj@truper.com>
2023-04-25 19:56:07 -0600

    Se agrega menu

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import Series from "./pages/Series";
 import Comic from "./pages/Comic";
 import Error404 from "./pages/Error404";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/inicio' element={<Home/>}/>
